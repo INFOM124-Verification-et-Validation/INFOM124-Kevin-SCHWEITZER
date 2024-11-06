103eddc8e6f0b47f0135efc4a7f5561b7e4b66b0
JonathanReid <55463001+ForsakenScheme@users.noreply.github.com>
2024-11-06 09:43:47 +0100

    fixed unittest

--- jpacman-framework/src/test/java/nl/tudelft/jpacman/board/UnitTest.java
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/board/UnitTest.java
@@ -10,22 +10,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UnitTest {
 
+    private BasicUnit unit;
+    private BasicSquare base;
+
+    @BeforeEach
+    public void setUp() {
+        // Create a BasicUnit and set its location to the base square (starting location for SquaresAheadOf
+        unit = new BasicUnit();
+        base = new BasicSquare();
+        unit.occupy(base);
+    }
+
     @Test
     public void testZeroSquaresAheadOfUnit() {
-        // Create a BasicUnit and set its location to the base square (starting location for SquaresAheadOf(0))
-        BasicUnit unit = new BasicUnit();
-        BasicSquare base = new BasicSquare();
-        unit.occupy(base);
         Square squareAheadIsBase = unit.squaresAheadOf(0);
         assertThat(squareAheadIsBase).isEqualTo(base);
     }
 
     @Test
     public void testOneSquaresAheadOfUnit() {
-        BasicUnit unit = new BasicUnit();
-        BasicSquare base = new BasicSquare();
         Square east1 = new BasicSquare();
-        unit.occupy(base);
         base.link(east1, Direction.EAST);
         Square square = unit.squaresAheadOf(1);
         assertThat(square).isEqualTo(east1);
@@ -33,9 +37,6 @@ public class UnitTest {
 
     @Test
     public void testTwoSquaresAheadOfUnit() {
-        BasicUnit unit = new BasicUnit();
-        BasicSquare base = new BasicSquare();
-        base.put(unit);
         Square east1 = new BasicSquare();
         base.link(east1, Direction.EAST);
         Square east2 = new BasicSquare();
@@ -46,9 +47,6 @@ public class UnitTest {
 
     @Test
     public void testNegativeSquaresAheadOfUnit() {
-        BasicUnit unit = new BasicUnit();
-        BasicSquare base = new BasicSquare();
-        unit.occupy(base);
         Square east1 = new BasicSquare();
         base.link(east1, Direction.EAST);
         Square west1 = new BasicSquare();
@@ -59,9 +57,6 @@ public class UnitTest {
 
     @Test
     public void testNoSquaresAheadOfUnit() {
-        BasicUnit unit = new BasicUnit();
-        BasicSquare base = new BasicSquare();
-        unit.occupy(base);
         Square square = unit.squaresAheadOf(2);
         assertThat(square).isEqualTo(base);
     }
