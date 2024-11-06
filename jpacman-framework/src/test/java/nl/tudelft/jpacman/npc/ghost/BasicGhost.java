3d980bc8fa52d75eca21a20bb9dd637ba5dfe0ee
JonathanReid <55463001+ForsakenScheme@users.noreply.github.com>
2024-11-06 14:07:33 +0100

    start

---
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/BasicGhost.java
@@ -0,0 +1,16 @@
+package nl.tudelft.jpacman.npc.ghost;
+
+class BasicGhost extends Ghost {
+
+    /**
+     * Creates a new ghost.
+     */
+    BasicGhost() {
+        super(GhostColor.RED, 200);
+    }
+
+    @Override
+    public void move() {
+        // Do nothing
+    }
+}
